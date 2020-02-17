03f86bfc73d9aed196a9a1204eba0857ac57d6ed
Bret Gibson <bret.gibson@gmail.com>
2020-02-17 21:13:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,6 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
-     
-     
 end
 
 def adding_to_dennis
@@ -129,5 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 
 end
