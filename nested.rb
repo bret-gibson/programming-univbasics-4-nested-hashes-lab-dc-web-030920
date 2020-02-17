3d283c56b13bc3233e436da8e7ceb3f6e808eb4f
Bret Gibson <bret.gibson@gmail.com>
2020-02-17 21:07:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
     programmer_hash[:dennis_ritchie][:languages].length.times do |index|
-      programmer_hash[:dennis_ritchie][:languages][index]
+      programmer_hash[:dennis_ritchie][:languages][index -1]
      end
 end
 
