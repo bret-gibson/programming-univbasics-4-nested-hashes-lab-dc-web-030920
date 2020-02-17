e4a629f66b0d83cbe94d7f495c9835075289cce9
Bret Gibson <bret.gibson@gmail.com>
2020-02-17 21:09:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,11 +66,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}
 	programmer_hash = 
  		{
  		    :yukihiro_matsumoto => {
